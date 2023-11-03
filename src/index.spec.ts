93c746d9a46e4d9046cb34a84401af70feb32724
Justin Lettau <justin.b.lettau@gmail.com>
2023-11-03 13:39:14 -0700

    chore: update dependencies

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,7 @@
-import states from '.';
+import states from './index';
 
 describe('states array', () => {
   it('contain all US states', () => {
-    expect(states.length).toBe(59);
+    expect(states.length).toBe(57);
   });
 });
